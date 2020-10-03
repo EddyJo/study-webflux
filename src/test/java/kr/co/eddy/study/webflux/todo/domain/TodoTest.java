408d085ea2973dd2ebe76cf6decb1907dca6c1cc
EddyJo <jkjk0600@gamil.com>
2020-10-03 20:56:58 +0900

    feat(java) : 테스트 주석 추가

--- src/test/java/kr/co/eddy/study/webflux/todo/domain/TodoTest.java
+++ src/test/java/kr/co/eddy/study/webflux/todo/domain/TodoTest.java
@@ -19,6 +19,8 @@ public class TodoTest {
         String dateTimeFormat = "yyyy-MM-dd HH:mm:ss";
         String expectedDateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateTimeFormat));
         Todo todo = Todo.builder().id(id).content(content).build();
+
+        //Test
         assertThat(todo).isInstanceOf(Todo.class);
         assertThat(todo.getId()).isEqualTo(id);
         assertThat(todo.getCreateDateTime().format(DateTimeFormatter.ofPattern(dateTimeFormat))).isEqualTo(expectedDateTime);
